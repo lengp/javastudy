a302115a8bf05ba3444e68003a5e289ab0b92613
lengpeng <lengpeng@i-counting.cn>
2019-09-12 17:59:55 +0800

    [feat] add test issue #3

--- src/main/java/com/example/demo/interceptor/BaseInterceptor.java
+++ src/main/java/com/example/demo/interceptor/BaseInterceptor.java
@@ -5,7 +5,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.HandlerMapping;
-import sun.misc.Request;
 
 /**
  * 拦截器.
