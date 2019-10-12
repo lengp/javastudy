d44f3da0fa8eeae192bf21c2c73b4c61988b4ba3
lengpeng <lengpeng@i-counting.cn>
2019-10-12 17:24:39 +0800

    [feat] 添加文档 issue #7

--- src/main/java/com/example/demo/controller/HelloWorld.java
+++ src/main/java/com/example/demo/controller/HelloWorld.java
@@ -1,6 +1,7 @@
 package com.example.demo.controller;
 
 import java.time.LocalDateTime;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,17 @@ public class HelloWorld {
 
   @GetMapping("/hello/{id}")
   public String helloWorldById(@PathVariable String id) {
-    return "Hello"+ id;
+    return "Hello" + id;
+  }
+
+  @Value("${my.name}")
+  private String name;
+
+  @Value("${my.age}")
+  private int age;
+
+  @GetMapping("/name")
+  public String myName() {
+    return name + ":" + age;
   }
 }
--- src/test/java/com/example/demo/controller/HelloWorldTest.java
+++ src/test/java/com/example/demo/controller/HelloWorldTest.java
@@ -1,17 +1,22 @@
 package com.example.demo.controller;
 
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.test.util.AssertionErrors.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.example.demo.DemoApplication;
 import org.junit.Assert;
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.restdocs.JUnitRestDocumentation;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -30,10 +35,14 @@ public class HelloWorldTest {
   @Autowired
   private WebApplicationContext context;
 
+  @Rule
+  public JUnitRestDocumentation restDocumentation = new JUnitRestDocumentation();
+
   @Before
   public final void setUp() {
 
     this.http = MockMvcBuilders.webAppContextSetup(this.context)
+        .apply(documentationConfiguration(this.restDocumentation))
         .alwaysDo(print())
         .build();
   }
@@ -56,5 +65,17 @@ public class HelloWorldTest {
         //.andExpect(content().string("HelloWorld"))
         .andDo(result -> assertEquals("", "Hello", result.getResponse().getContentAsString().substring(0, 5)));
   }
+
+
+  @Test
+  public void getName() throws Exception {
+    http.perform(get("/name"))
+        .andExpect(status().isOk())
+        .andExpect(content().string("lengpeng:22"))
+        .andDo(
+            document("get-name")
+        );
+  }
+
 
 }
